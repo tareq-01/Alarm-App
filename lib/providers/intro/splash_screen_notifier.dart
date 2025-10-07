13b27bd85ff6574525e79a7a980b5edbf2ec63a3
Md Tariqul Islam <genie.tariqul@gmail.com>
2025-10-07 19:37:11 +0600

    six

--- lib/providers/intro/splash_screen_notifier.dart
+++ lib/providers/intro/splash_screen_notifier.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:alarm_app/providers/alarm/alarm_page_notifier.dart';
 import 'package:alarm_app/services/app_route.dart';
+import 'package:alarm_app/services/constants/alarm_model/alarm_model.dart';
 import 'package:alarm_app/services/constants/auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,7 +13,8 @@ class SplashScreenNotifier extends StateNotifier<dynamic> {
   }
   void timeDuration() async {
     final alarmPageNotifier = ref.read(alarmPageProvider.notifier);
-  await AuthUtility().getAlarm(); 
+  List<AlarmModel> alarmList=  await AuthUtility().getAlarm(); 
+  
     Timer(Duration(seconds: 2), () => router.push("/home"));
   }
 }
